a2e8517e9b8bc911a69d5d8d50d67a5377ef62ba
bbarr <bobbyb9949@outlook.com>
2019-05-14 15:50:22 -0500

    Initial check in

---
+++ Ch2/Average Numbers1/src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	double a;
+	double b;
+	double c;
+	double d;
+	a = 4.5;
+	b = 13.4;
+	c = 12.3;
+	d = 42.0;
+	int average;
+	average = a+b+c+d%4;
+        System.out.println("The average is: ");
+    }
+}

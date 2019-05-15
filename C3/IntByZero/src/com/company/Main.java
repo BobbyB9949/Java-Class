34b06d37e6fab00c4f1bfe1fb4467e3f6b61f228
bbarr <bobbyb9949@outlook.com>
2019-05-15 16:36:19 -0500

    commit exercises

---
+++ C3/IntByZero/src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    int x = 42;
+    int y = 0;
+    int result;
+    result = x /y;
+          System.out.println("Result: " + (x / y));
+
+
+}
+}

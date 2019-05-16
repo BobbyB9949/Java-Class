59227fa7bc259a99b2243356c2d3f5f1e2fcb00d
bbarr <bobbyb9949@outlook.com>
2019-05-16 15:37:56 -0500

    commiting PrintLargest

---
+++ C4/PrintLargest/src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+        int number1 = 3;
+        int number2 = 4;
+        if (number1 == number2)
+           System.out.println("The numbers are equal.");
+        else if (number1 >= number2);
+        System.out.println("The largest number is: " + number1);
+        if (number2 >= number1)
+        System.out.println("The largest number is: " + number2);
+
+
+    }
+}

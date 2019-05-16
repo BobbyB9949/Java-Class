f4cdaa65748bfdcee61dc8edb1ff6cc15d4071da
bbarr <bobbyb9949@outlook.com>
2019-05-16 14:21:31 -0500

    committing

---
+++ C4/EvenOrOdd/src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+        int number = 97;
+
+        if (number % 2 == 0)
+        {
+            System.out.println("The number is even");
+        }
+        else
+        {
+            System.out.println("The number is odd");
+        }
+
+
+    }
+}

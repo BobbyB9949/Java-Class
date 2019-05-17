55545c49108f956a38dd3cdf6d2480d71361a122
bbarr <bobbyb9949@outlook.com>
2019-05-17 16:01:08 -0500

    commit file

---
+++ C4/PrintSequence/src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int num = 100;
+
+
+        while (num >= -100)
+        {
+            System.out.println(num);
+            num -= 8;
+        }
+
+
+    }
+}
